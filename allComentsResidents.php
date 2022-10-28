180c94ed506861f53ac016d6220987a2ff189c76
Prabiella <paquirrirabiella@gmail.com>
2022-10-27 19:34:03 -0500

    ajustes

---
+++ allComentsResidents.php
@@ -0,0 +1,14 @@
+<?php
+  $conexion= new mysqli('us-cdbr-east-06.cleardb.net','bab1268ab5e278','ce1a7eef','heroku_e021dfd5e5644c0');
+  //$conexion = mysqli_connect('localhost','root','','commentsadmin');
+ 
+    $myArray = array();
+
+    $sql = "SELECT * FROM notificationresidents";
+    $resultado = mysqli_query($conexion, $sql);
+  
+    while($row = $resultado->fetch_array(MYSQLI_ASSOC)) {
+        $myArray[] = $row;
+}
+echo json_encode($myArray);    
+?>
--- createNotificationResidents.php
+++ createNotificationResidents.php
@@ -1,7 +1,7 @@
 <?php
 //mysql://bab1268ab5e278:ce1a7eef@us-cdbr-east-06.cleardb.net/heroku_e021dfd5e5644c0?reconnect=true
-//$conexion= new mysqli('us-cdbr-east-06.cleardb.net','bab1268ab5e278','ce1a7eef','heroku_e021dfd5e5644c0');
-$conexion = mysqli_connect('localhost','root','','commentsadmin');
+$conexion= new mysqli('us-cdbr-east-06.cleardb.net','bab1268ab5e278','ce1a7eef','heroku_e021dfd5e5644c0');
+//$conexion = mysqli_connect('localhost','root','','commentsadmin');
 	$notification=$_POST['notification'];
     $type = $_POST['type'];
 	$message=$_POST['message'];
@@ -12,7 +12,7 @@ if ($conexion->connect_error) {
 }
 
 // sql Crea la tabla usando Lenguaje PHP
-$sql = "CREATE TABLE IF NOT EXISTS notificationAdmin (
+$sql = "CREATE TABLE IF NOT EXISTS notificationresidents (
 id INT(150) NOT NULL AUTO_INCREMENT PRIMARY KEY,
 notification VARCHAR(150) NOT NULL,
 type VARCHAR(150) NOT NULL,
@@ -26,7 +26,7 @@ if ($conexion->query($sql) === TRUE) {
     echo "Hubo un error al crear la tabla comentarios: " . $conn->error;
 }
 
-	$sql="INSERT into notificationAdmin (notification,type,message)
+	$sql="INSERT into notificationresidents (notification,type,message)
 			values ('$notification','$type','$message')";
 	echo mysqli_query($conexion,$sql);
 ?>
